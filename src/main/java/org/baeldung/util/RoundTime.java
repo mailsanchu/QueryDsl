5e331aa6d8f20c2edc0fd709896c804cf7e1032c
Sanchu Varkey <svarkey@digisoft.tv>
2019-02-16 23:53:45 +0000

    Add main method to test

--- src/main/java/org/baeldung/util/RoundTime.java
+++ src/main/java/org/baeldung/util/RoundTime.java
@@ -1,7 +1,12 @@
 package org.baeldung.util;
 
+import java.time.Instant;
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BiPredicate;
+
+import static java.time.Instant.ofEpochMilli;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 public class RoundTime {
 
@@ -21,6 +26,16 @@ public class RoundTime {
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         return calendar;
+    }
+
+    public static void main(String[] args) {
+        long startTimeOriginal = System.currentTimeMillis();
+        long endTimeOriginal = TimeUnit.HOURS.toMillis(6) + startTimeOriginal;
+        System.out.println(ofEpochMilli(startTimeOriginal).truncatedTo(SECONDS) + " || " + ofEpochMilli(endTimeOriginal).truncatedTo(SECONDS));
+        Calendar startCalender = RoundTime.getCalendar(startTimeOriginal, 30);
+        Calendar endCalender = RoundTime.getCalendar(endTimeOriginal, 30);
+        Instant x = ofEpochMilli(truncateTime((a, b) -> a <= b, startCalender.getTimeInMillis(), startTimeOriginal, startCalender, -1));
+        System.out.println(x + " || " + ofEpochMilli(truncateTime((a, b) -> a > b, endCalender.getTimeInMillis(), endTimeOriginal, endCalender, 1)));
     }
 
 }
